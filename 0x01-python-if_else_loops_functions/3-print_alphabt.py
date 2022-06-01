e51e15a6ddd6027caccd843bd17962745d5032a6
jobnovice <jobkebede47@gmail.com>
2022-06-01 18:13:56 +0000

    yeah man

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in "abcdefghijklmnopqrstuvwxyz":
-    if i !=4 or i !=16:
-        print('{}'.format(i), end="")
-    else:
-        continue
+    if i != "q" and i != "e":
+        print("{}".format(i), end="")
