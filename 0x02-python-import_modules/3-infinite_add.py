9f5aa0077200cb7d48d9d1885e0754c688456100
Eyob <jobkebede47@gmail.com>
2022-06-03 00:41:49 +0300

    esti demo

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    n = 0
+    count = len(sys.argv)
+    for i in range(count):
+        n += int(argv[i])
+    print("{}".format(n))
