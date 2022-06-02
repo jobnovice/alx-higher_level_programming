a2d097a3c73246bfd2fa53e40bc0474cae3655f9
Eyob <jobkebede47@gmail.com>
2022-06-03 00:30:42 +0300

    pus

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    if (len(sys.argv)) == 1:
-        print("0 arguments")
-    elif (len(sys.argv) == 2):
-        print("{} argumnet\n1: {}".format(len(sys.argv) - 1, str(sys.argv[1])))
+    
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        for i in range (len(sys.argv)):
-            print("{} argumnets\n{}: {}".format(len(sys.argv),i + 1, str(sys.argv[i])))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
