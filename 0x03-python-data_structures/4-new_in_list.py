992387ab01408f27c7cc7a2bb62fe25c17a3713d
jobnovice <jobkebede47@gmail.com>
2022-06-07 20:10:50 +0000

    some latches

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    List = my_list
-    if idx < 0 or idx >= len(my_list):
-        return List
-    else:
-        List[idx] = element
-        return List
+    copy = my_list.copy()
+    for x in my_list:
+        if idx == my_list.index(x):
+            copy[idx] = element
+            return (copy)
+        elif idx >= len(my_list):
+            return (copy)
+        elif idx < 0:
+            return (copy)
