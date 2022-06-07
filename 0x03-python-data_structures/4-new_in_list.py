3f60ff316c484aa394581aa04b257599069e1b24
jobnovice <jobkebede47@gmail.com>
2022-06-07 19:04:19 +0000

    jj

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    List = my_list
+    if idx < 0 or idx >= len(my_list):
+         return List
+     else:
+         List[idx] = element 
+        return List
