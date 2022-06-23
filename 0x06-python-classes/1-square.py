130049d04a71ded8e2ecd76bbd122e4690f1284b
jobnovice <jobkebede47@gmail.com>
2022-06-23 19:48:05 +0000

    esti

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,16 +2,19 @@
 
 
 """
-    created the class sqaure with one parametes
+    created the class sqaure with one parameters
     Args:
         size:the description of the size
 """
 
 
 class Square:
+    """does this needs to be here every time. """
+
     def __init__(self, size):
         """ or here it could also be here
             Args:
                 size:is the one that is used to intialize
         """
+
         self.__size = size
