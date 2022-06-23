cde344ae07edfd3bc760aea414e3defe59c12b4a
Eyob <jobkebede47@gmail.com>
2022-06-23 21:57:42 +0300

    yeah with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+""" created the class sqaure with one parametes
+    Args:
+        size:the description of the size    
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """ t"""
+        self.__size = size
