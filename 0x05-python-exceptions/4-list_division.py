d8bfea9a2d029ccdb16014d31030edc5e3f4be8b
jobnovice <jobkebede47@gmail.com>
2022-06-30 18:50:26 +0000

    mmh

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,4 @@
 #!/usr/bin/pyhton3
-from sqlite3 import adapt
-
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [None] * list_length
     for i in range(0, list_length):
@@ -14,7 +11,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
         finally:
-            if my_list_1[i] % my_list_2[i] is not 0:
-                new_list[i] = 0 
             new_list[i] = my_list_1[i] / my_list_2[i]
     return new_list
