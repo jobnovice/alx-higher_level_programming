55c67440d65fecabb70e2457caa892ae7de58a7a
jobnovice <jobkebede47@gmail.com>
2022-07-05 11:27:23 +0000

    2:46 ops!

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 ''' yeah automatically filled it with string documentation'''
 
+
 class Square:
     ''' the class with square and it's specfiction'''
-    def __int__(self, size = 0):
-        ''' yeah the constructor needs to 
+    def __int__(self, size=0):
+        '''yeah the constructor needs to
         introduce us it's arguments
             Args:
                 size = the size of the square
@@ -14,5 +15,8 @@ class Square:
         elif size < 0:
             raise ValueError("size must >= 0")
         self.__size = size
+
     def area(self):
+        '''the method to calculate and return the area of teh square
+        '''
         return self.__size * self.__size
