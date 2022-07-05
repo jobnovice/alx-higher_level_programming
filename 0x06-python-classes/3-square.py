8f31fa773b185ab69f7e50b7fb88133a0edcb454
jobnovice <jobkebede47@gmail.com>
2022-07-05 18:52:18 +0000

    i dont think so

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-''' yeah automatically filled it with string documentation'''
+""" yeah automatically filled it with string documentation"""
 
 
 class Square:
-    ''' the class with square and it's specfiction'''
+    """ the class with square and it's specfiction"""
 
     def __int__(self, size=0):
-        '''yeah the constructor needs to introduce us it's arguments
+        """yeah the constructor needs to introduce us it's arguments
             Args:
                 size (int): the size of the square
             Returns:
                 None
-        '''
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -19,6 +19,6 @@ class Square:
         self.__size = size
 
     def area(self):
-        '''Returns: 
-            the current area of the squaree'''
-        return (self.__size * self.__size)
+        """Returns: 
+            the current area of the squaree"""
+        return self.__size * self.__size
