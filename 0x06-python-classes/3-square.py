9a8ed2db54dc8cc22a8a27a1c82a8179448a3fa8
jobnovice <jobkebede47@gmail.com>
2022-07-05 17:31:04 +0000

    8:30

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,19 +4,19 @@
 
 class Square:
     ''' the class with square and it's specfiction'''
-    def __int__(self, size=0):
+   
+   def __int__(self, size=0):
         '''yeah the constructor needs to
         introduce us it's arguments
             Args:
-                size = the size of the square
+                size (int): the size of the square
         '''
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must >= 0")
         self.__size = size
 
     def area(self):
-        '''the method to calculate and return the area of teh square
-        '''
+        '''Return the current area of the squaree'''
         return (self.__size * self.__size)
