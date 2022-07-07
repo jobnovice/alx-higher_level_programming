6da31b756bc6e60477fa53d626f9268c75e35933
Eyob <jobkebede47@gmail.com>
2022-07-07 23:19:57 +0300

    esti let's check this one out

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,7 +1,33 @@
+#!/usr/bin/python3
 '''Class square'''
+
+
 class square:
-    ''' '''
-    def __init__(self, value):
-        pass
-    def get_self(self, value):
-      pass  
+    '''class square '''
+
+    def __init__(self, size=0):
+        ''' well constructor
+            Args:
+                size (int): well it's an int
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    @property 
+    def size(self):
+        ''' do we need to documment this or should i leave it like that
+            Return the deafult size
+        '''
+        return self.__size
+    @size.setter
+    def size(self, value):
+        ''' yeah man 
+            Args:
+                value (int): the value to change the size of square with
+        '''
+        self.__size = value
+    def area(self):
+        '''Return the are of teh square'''
+        return (self.__size * self.__size)
