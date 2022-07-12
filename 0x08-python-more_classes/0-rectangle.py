30d0261fbe6efcaea1608907750ffe3c27125471
jobnovice <jobkebede47@gmail.com>
2022-07-12 20:35:44 +0000

    11:35

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''new class Rectangle defined'''
+'''new class Rectangle defined"""
 
 
 class Rectnagle:
-    '''yeah again defined the new class rectangle i think we solved it
-    '''
+    """yeah again defined the new class rectangle i think we solved it
+    """
     pass
