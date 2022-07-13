521386e51d6c60f982e793e382317ba204ad03ff
jobnovice <jobkebede47@gmail.com>
2022-07-13 19:28:01 +0000

    done with this part

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,16 +5,17 @@
 class Rectangle:
     """new class Rectangle defined but it's empty for now"""
     def __init__(self, width=0, height=0):
-        """ yeah module is documented 
+        """ yeah module is documented
             Args:
                 width (int): the width of the rectangle
                 height (int): the height of the rectangle"""
         self.width = width
         self.height = height
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         '''to set the value of the heigh
@@ -24,11 +25,12 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value   
+        self.__height = value
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         '''to set the value of the heigh
@@ -38,4 +40,4 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value   
+        self.__width = value
