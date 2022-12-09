cfb9b88e1aba2fee9916bb604f9c5b5c389d32bb
Eyob <jobkebede47@gmail.com>
2022-12-09 16:05:19 +0300

    wee

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{:s}".format(chr(i - 32) if i % 2 else chr(i)), end="")
+for i in range(122, 98, -1):
+    print("{:c}".format(chr(i - 32) if i % 2 else i), end="")
