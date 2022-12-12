7516d37fd639034ccee7135d7b030ea2e35fa7e8
jobnovice <jobkebede47@gmail.com>
2022-12-12 18:32:32 +0000

    new tnss

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1.py import add, mul, div, sub
+    from calculator_1 import add, mul, div, sub
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
