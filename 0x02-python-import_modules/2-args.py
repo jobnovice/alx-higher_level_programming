0f148da8054d947fa002504f5a6fdf8a8d92aa53
jobnovice <jobkebede47@gmail.com>
2022-12-12 19:10:11 +0000

    hold on

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
     i = len(sys.argv)
     if i == 1:
         print("0 arguments.")
+    elif i == 2:
+        print("{:d} argument:".format(i - 1))
+        for l in range(1, i):
+            print("{}: {}".format(l, sys.argv[l]))
     else:
-        print("{:d} arguments:".format(i - 1))
+        print("{:d} arguments".format(i - 1))
         for l in range(1, i):
             print("{}: {}".format(l, sys.argv[l]))
