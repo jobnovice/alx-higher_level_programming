f044a2eddcd41f5c1706211d7f5fdfa24e74c6ef
Eyob <jobkebede47@gmail.com>
2022-12-14 21:53:53 +0300

    do some math

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,12 @@
 if __name__ == "__main__":
     import sys
     num = len(sys.argv)
+    res = 0
     if num == 1:
-        print("")
+        print("0")
     elif num == 2:
-        print("")
-        
+        print("{}".format(int(sys.argv[1])))
+    else:
+        for i in range(1, num + 1):
+            res += int(sys.argv[i])
+        print("{}".format(res))
