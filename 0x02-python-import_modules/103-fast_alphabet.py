a25f6e4d53b5a5a45071cba14907a47163d99081
Eyob <jobkebede47@gmail.com>
2022-12-16 01:06:03 +0300

    one last kick

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+from me import prins
+prins()
--- 0x02-python-import_modules/me.py
+++ 0x02-python-import_modules/me.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import dis
-
-for i in range(4, 6):
-    print("me is right.")
-dis.dis(me)
+def prins():
+    for i in range(65, 91):
+        print(chr(i),end="")
+    return None
