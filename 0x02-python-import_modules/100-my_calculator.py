320ca2b82f87bfb6e63a2930e445697105162d4a
Eyob <jobkebede47@gmail.com>
2022-12-15 14:08:47 +0300

    no hits

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,23 +4,24 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv)
     
-
     if num != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         if sys.argv[2] == '+':
-            print("{} + {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), (int(sys.argv[1]) + int(sys.argv[3]))))
+            print("{} + {} = {}".format(a, b, (a + b)))
             exit(0)
         elif sys.argv[2] == '*':
-            print("{} + {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), (int(sys.argv[1]) * int(sys.argv[3]))))
+            print("{} + {} = {}".format(a, b, (a * b)))
             exit(0)
         elif sys.argv[2] == '-':
-            print("{}".format(int(sys.argv[1]), int(sys.argv[3]), (int(sys.argv[1]) - int(sys.argv[3]))))
+            print("{} + {} = {}".format(a, b, (a - b)))
             exit(0)
         elif sys.argv[2] == '/':
-            print("{}".format(int(sys.argv[1]), int(sys.argv[3]), (int(sys.argv[1]) / int(sys.argv[3]))))
+            print("{} + {} = {}".format(a, b, (a / b)))
             exit(0)
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)  
+            exit(1)
