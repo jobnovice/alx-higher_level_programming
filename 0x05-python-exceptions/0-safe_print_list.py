58f4c8f645bb0a5eee227376dbffe11ad76fd53a
jobnovice <jobkebede47@gmail.com>
2022-12-19 21:06:06 +0000

    40 dia

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         while i < x:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             i += 1
-    except Exception:
+        print()
+        return i
+    except:
+        print()
         pass
