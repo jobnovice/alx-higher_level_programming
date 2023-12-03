b042a04c08c5927ac81c526a59f0400120b11be4
jobnovice <jobkebede47@gmail.com>
2023-12-03 21:39:03 +0300

    wc -l == 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 #a bash script that accepts a URL
-if [$# -eq 0];then
-    echo "Usage: $0 <url>"
-    exit 1
-fi
-
-url=$1
-
-
-body_size = $(curl -sI $url | grep -i content-length)
-echo "$body_size"
+echo = $(curl -sI "$1" | grep -i content-length)
