51d4f95908fb0c6f19259b4b48961f8202f8fe6a
jobnovice <jobkebede47@gmail.com>
2023-12-05 00:00:57 +0300

    wht abt this

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #another request from us to the server with a few variables sent along with
-curl -sH "email: test@gmail.com" -H "subject: I will always be here for PLD" "$1"
+curl -sH "email: test@gmail.com" -H "subject: I will always be here for PLD" $1
