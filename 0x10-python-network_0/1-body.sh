dab55380b024c1121e11fdcd7427670fbced320c
jobnovice <jobkebede47@gmail.com>
2023-12-04 22:27:50 +0300

    wc -l ==3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request for a given url
+# sends a GET request for a given url and returns the reposne body only if the http status coed is 200
 curl -sIX "GET" $1 | awk '/^HTTP/ {print $2}'
