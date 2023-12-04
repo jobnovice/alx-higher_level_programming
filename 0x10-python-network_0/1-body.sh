18362ee951b3b368df9c7f5624855797cc6b6d27
jobnovice <jobkebede47@gmail.com>
2023-12-04 22:32:00 +0300

    chtt

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request for a given url and returns the reposne body only if the http status coed is 200
-curl -sIX "GET" $1 | awk '/^HTTP/ {print $2}'
+curl -s -w "\n%{http_code}" "$1" | awk '/^$/{p=0} p; /./{p=1}'
