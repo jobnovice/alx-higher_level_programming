411b24a1d3098bebf3b6b424f18f186a55443e75
jobnovice <jobkebede47@gmail.com>
2023-12-06 01:24:14 +0300

    modifed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,11 @@
 if __name__ == '__main__':
     import urllib.request
     import sys
-    rs = urllib.request.Request(sys.argv[1],data="email:{sys.argv[2]}" , method = "POST")
-    with urllib.request.urlopen(rs) as response:
-        body = response.read().decode("utf-8")
-        print("your email  is: ", body.headers["email"])
+    import urllib.parse
+
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
