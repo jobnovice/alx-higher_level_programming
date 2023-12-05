e6de1ee0146fccbd167c4afc6de79c0eb6ff8c27
jobnovice <jobkebede47@gmail.com>
2023-12-05 22:16:11 +0300

    mifirst1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,6 @@
 """a script that takes a url as an argument and prints the response header"""
 import urllib.request
 import sys
-rs = sys.argv[1]
+rs = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(rs) as reponse:
     print(reponse.headers['X-Request-Id'])
