7ae3c5e55a2a1db6d488c2b9cf980a1f56eadb28
jobnovice <jobkebede47@gmail.com>
2023-12-05 21:13:04 +0300

    wht abt this

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,10 @@
 # a scirpt that fetches a given URL
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read().decode('utf-8')
+    content_bytes = response.read()
+    content_str = content_bytes.decode('utf-8')
 
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- type:", body)
+print("Body response:")
+print("\t- type: {}".format(type(content_bytes)))
+print("\t- content: {}".format(content_bytes))
+print("\t- utf8 content: {}".format(content_str))
