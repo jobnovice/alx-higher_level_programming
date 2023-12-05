ba74308898109aa6fbcbf892fcd02ebaf6900a25
jobnovice <jobkebede47@gmail.com>
2023-12-05 20:53:58 +0300

    abt  time

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # a scirpt that fetches a given URL
-import urlib.request
-with urlib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     the_page = response.read()
-    
