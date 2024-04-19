13dc41460d2582a049a91c655361e48b83b9d562
jobnovice <jobkebede47@gmail.com>
2024-04-20 01:52:44 +0300

    semistandard --fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,15 +4,15 @@ const fs = require('fs');
 
 // check if the file and the string to write is passed to out script
 if (process.argv.length !== 4) {
-	console.error('Usage: 1-writeme.js filepath StringToWrite');
-	process.exit(1);
+  console.error('Usage: 1-writeme.js filepath StringToWrite');
+  process.exit(1);
 }
 
 const stringToWrite = process.argv[3];
 const filepath = process.argv[2];
 
 fs.writeFile(filepath, stringToWrite, 'utf-8', (err) => {
-	if (err){
-		process.exit(1);
-	}
+  if (err) {
+    process.exit(1);
+  }
 });
