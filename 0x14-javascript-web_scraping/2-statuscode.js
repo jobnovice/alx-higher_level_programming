53344274117afdb5e88e129114ce294f57eab90c
jobnovice <jobkebede47@gmail.com>
2024-04-20 02:12:15 +0300

    mm2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,7 +7,6 @@ if (process.argv.length !== 3) {
 }
 
 const url = process.argv[2];
-request(url, function (error, response) {
-  console.error(error);
+request(url, function (response) {
   console.log('code:', response.statusCode); // Print the response status code
 });
