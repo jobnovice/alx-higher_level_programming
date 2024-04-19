077892018f8d98f557a2cfea40925df14f5ca2a9
jobnovice <jobkebede47@gmail.com>
2024-04-20 01:30:32 +0300

    aight

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 // check if the file is passed as an argument to our script
-if (process.argv.length < 3) {
+if (process.argv.length !== 3) {
   console.error('Usage: 0-readme.js <filepath>');
   process.exit(1);
 }
@@ -10,12 +10,11 @@ if (process.argv.length < 3) {
 // load the file from the argument
 const filepath = process.argv[2];
 
-// read the file asynchronously
-fs.readFileSync(filepath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error:', err);
-    process.exit(1);
-  }
-  // if no error encounterd print the file
-  console.log(data);
-});
+// read the file synchronously
+const data = fs.readFileSync(filepath, 'utf-8');
+if (!data) {
+  console.error('Error: Unable to read file:', filepath);
+  process.exit(1);
+}
+
+console.log(data);
