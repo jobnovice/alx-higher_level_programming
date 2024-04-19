d1b58282fd7f940bbedb3c7ae86062e22805910b
jobnovice <jobkebede47@gmail.com>
2024-04-20 01:46:39 +0300

    airr

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// check if the file and the string to write is passed to out script
+if (process.argv.length !== 4) {
+	console.error('Usage: 1-writeme.js filepath StringToWrite');
+	process.exit(1);
+}
+
+const stringToWrite = process.argv[3];
+const filepath = process.argv[2];
+
+fs.writeFile(filepath, stringToWrite, 'utf-8', callback);
