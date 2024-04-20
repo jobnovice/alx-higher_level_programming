f12100ab10b1373efea73aa61d864778e78c968c
jobnovice <jobkebede47@gmail.com>
2024-04-20 03:52:29 +0300

    mmh

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,16 +2,12 @@
 const request = require('request');
 const fs = require('fs');
 
-if (process.argv.length !== 4){
-	console.log('Usage: 5-request_store.js URL filepath');
-	process.exit(1);
+if (process.argv.length !== 4) {
+  console.log('Usage: 5-request_store.js URL filepath');
+  process.exit(1);
 }
 
 const url = process.argv[2];
 const filepath = process.argv[3];
 
-request(url).pipe(fs.createReadStream(filepath, 'utf-8', (err) => {
-	if (err){
-		process.exit(1);
-	}
-}));
+request(url).pipe(fs.createWriteStream(filepath, 'utf-8'));
