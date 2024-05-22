417c099f9c0ecb4e6370aba3730e580c112a439f
jobnovice <jobkebede47@gmail.com>
2024-05-22 19:30:58 +0300

    let's push it

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""create a class called myList that inherits from list"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """prints the list in a ascending order"""
+        sorted_list = sorted(self)
+        print(sorted_list)
+
+
+# my_list = MyList([2, 4, 5, 6, 1])
+
+# print(my_list)
+# my_list.print_sorted()
