7688554f59366c4f90ee60dc571cff610e3405a2
jobnovice <jobkebede47@gmail.com>
2024-05-23 19:30:40 +0300

    haven't pushed

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""improves the previous implemented version of the geometry class"""
+
+
+class BaseGeometry:
+    """slight improvement made to the previouse geometry"""
+    def area(self):
+        """returns the area of the geometry"""
+        raise Exception("area() is not implemented")
