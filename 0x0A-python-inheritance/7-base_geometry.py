20b8cc3f97efb9e73907fae2f691798a0cdfe62f
jobnovice <jobkebede47@gmail.com>
2024-05-24 01:27:39 +0300

    ll

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        return True
+
 
 # b1 = BaseGeometry()
 # BaseGeometry.area()
