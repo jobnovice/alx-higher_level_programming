a99e9acad3f1a21f615ea33ced96c15f63ac9ed9
jobnovice <jobkebede47@gmail.com>
2024-05-25 00:34:08 +0300

    Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,16 +2,20 @@
 """new class Square created that inheirts from Rectangle"""
 
 
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square:
+class Square(Rectangle):
     """square class created """
     def __init__(self, size):
         """instantiates square class"""
         self.integer_validator("size", size)
         self.__size = size
-    
+        super().__init__(size, size)
+
     def area(self):
         """returns the area of the square object"""
         return (self.__size * self.__size)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
