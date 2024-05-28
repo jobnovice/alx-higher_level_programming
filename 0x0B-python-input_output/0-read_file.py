f7b536bf27beea1b3033a8fc1b3318432e5e5c91
jobnovice <jobkebede47@gmail.com>
2024-05-28 17:16:38 +0300

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""reading file contents"""
+
+
+def read_file(filename=""):
+	"""reads file content in a utf-8 format
+		Args:
+			filename: string representing filename
+		Returns: nothing	
+	"""
+	with open(filename, encoding='utf-8') as f:
+		print(f.read())
