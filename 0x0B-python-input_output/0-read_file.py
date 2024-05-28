fbd07d3bb63b30abcc317b7921365dbffcb0abae
jobnovice <jobkebede47@gmail.com>
2024-05-28 17:18:35 +0300

    niche

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,10 +3,10 @@
 
 
 def read_file(filename=""):
-	"""reads file content in a utf-8 format
-		Args:
-			filename: string representing filename
-		Returns: nothing	
-	"""
-	with open(filename, encoding='utf-8') as f:
-		print(f.read())
+    """reads file content in a utf-8 format
+        Args:
+            filename: string representing filename
+        Returns: nothing
+    """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
