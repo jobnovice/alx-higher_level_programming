6bf696343e1029416d0506e776841db9bf19f004
jobnovice <jobkebede47@gmail.com>
2024-05-30 03:28:35 +0300

    nn

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,31 +16,8 @@ class Student():
                 attrs: optional list of attributes
             returns: a json representaion of the object
         """
-        dict1 = {}
-        if attrs is not None and isinstance(attrs, list) and len(attrs) > 0:
-            for attr in attrs:
-                if hasattr(self, attr):
-                    value = getattr(self, attr)
-                    if isinstance(value, str):
-                        value = value.replace("'", "").replace('"', "")
-                    dict1[attr] = value
-        elif attrs == []:
-            return {}
+        if attrs is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attrs}
+            return res
         else:
             return self.__dict__
-
-        return dict1
-
-
-student_1 = Student("John", "Doe", 23)
-student_2 = Student("Bob", "Dylan", 27)
-
-j_student_1 = student_1.to_json()
-j_student_2 = student_2.to_json(['first_name', 'age'])
-j_student_3 = student_2.to_json(['middle_name', 'age'])
-j_student_4 = student_1.to_json([])
-
-print(j_student_1)
-print(j_student_2)
-print(j_student_3)
-print(j_student_4)
