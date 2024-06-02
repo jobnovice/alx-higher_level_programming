c933774231bdb9945726bdd109aea64dc721a7c2
jobnovice <jobkebede47@gmail.com>
2024-06-03 01:11:18 +0300

    hope this fixs it

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module that defines the Rectangle class, inheriting from the Base class."""
+"""
+Module that defines the Rectangle class, inheriting from the Base class.
+"""
+
 from base import Base
 
 
