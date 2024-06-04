54de3f25a777ff54413a1dc7907b098e566d9e83
jobnovice <jobkebede47@gmail.com>
2024-06-04 19:25:24 +0300

    push it

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the string representation of a list of dictionaries"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
@@ -26,6 +26,6 @@ class Base:
     def save_to_file(cls, list_objs):
         """saves the list of dictionaries to a file"""
         filename = "{}.json".format(cls.__name__)
-        dicts = cls.to_json_string(i.to_dictionary() for i in list_objs)
+        dicts = cls.to_json_string([i.to_dictionary() for i in list_objs])
         with open(filename, 'w') as f:
             f.write(dicts)
