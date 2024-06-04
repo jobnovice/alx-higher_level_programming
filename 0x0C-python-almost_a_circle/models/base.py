8151414a2079570adddc27cea2df887da5229b75
jobnovice <jobkebede47@gmail.com>
2024-06-04 13:26:59 +0300

    gravity

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""created the firs class"""
+"""created the base class"""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the string representation of a list of dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
