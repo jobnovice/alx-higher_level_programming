b0c6e6868da4733afa3480269a0644345527cadc
jobnovice <jobkebede47@gmail.com>
2024-06-04 20:32:18 +0300

    pss

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,10 @@ class Base:
         dicts = cls.to_json_string([i.to_dictionary() for i in list_objs])
         with open(filename, 'w') as f:
             f.write(dicts)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """converts the json string to dicitonary"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
