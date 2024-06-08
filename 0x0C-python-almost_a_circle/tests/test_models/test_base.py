83a1dcd3a46328e8e07653961faa68ddbb4261d3
jobnovice <jobkebede47@gmail.com>
2024-06-08 13:27:13 +0300

    one by one

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -34,7 +34,21 @@ class Testbaseclass(unittest.TestCase):
         self.assertEqual(Base.from_json_string('[{ "id": 89 }]'), )
 
 #     --------- private attribute of class --------
-
+    def testsetid(self):
+        """ test for set id"""
+        b = Base("h")
+        b1 = Base({1: 2})
+        b2 = Base((1, 4))
+        b3 = Base([3, 6])
+        b4 = Base(None)
+        b5 = Base(0)
+        self.assertEqual(b.id, "h")
+        self.assertEqual(b1.id, {1: 2})
+        self.assertEqual(b2.id, (1, 4))
+        self.assertEqual(b3.id, [3, 6])
+        self.assertEqual(b4.id, 4)
+        self.assertEqual(b5.id, 0)
+    
     def testprivate(self):
         """ test for private attribute"""
         b = Base()
@@ -43,52 +57,52 @@ class Testbaseclass(unittest.TestCase):
 
 # #    -------------- static method to_json_string -----------
 
-    def testtojson(self):
-        """ test to json"""
-        dic = {'id': 8, 'size': 5, 'x': 6, 'y': 7}
-        stdic = json.dumps([dic])
-        self.assertEqual(Base.to_json_string([dic]), stdic)
-        r = Base.to_json_string(None)
-        self.assertEqual(r, "[]")
-        r = Base.to_json_string([])
-        self.assertEqual(r, "[]")
-        dic = [1, 2, 3]
-        r = Base.to_json_string([dic])
-        self.assertEqual(r, "[[1, 2, 3]]")
+#     def testtojson(self):
+#         """ test to json"""
+#         dic = {'id': 8, 'size': 5, 'x': 6, 'y': 7}
+#         stdic = json.dumps([dic])
+#         self.assertEqual(Base.to_json_string([dic]), stdic)
+#         r = Base.to_json_string(None)
+#         self.assertEqual(r, "[]")
+#         r = Base.to_json_string([])
+#         self.assertEqual(r, "[]")
+#         dic = [1, 2, 3]
+#         r = Base.to_json_string([dic])
+#         self.assertEqual(r, "[[1, 2, 3]]")
 
-    def testtojson1(self):
-        """error json"""
-        with self.assertRaises(TypeError):
-            Base.to_json_string()
+#     def testtojson1(self):
+#         """error json"""
+#         with self.assertRaises(TypeError):
+#             Base.to_json_string()
 
 # #  --------------  save to file -------------------------
 
-    def testsavetofile(self):
-       """ test save to file"""
-       Base.save_to_file([])
-       with open("Base.json") as fd:
-            self.assertEqual(fd.read(), "[]")
-       Base.save_to_file(None)
-       with open("Base.json") as fd:
-            self.assertEqual(fd.read(), "[]")
+    # def testsavetofile(self):
+    #    """ test save to file"""
+    #    Base.save_to_file([])
+    #    with open("Base.json") as fd:
+    #         self.assertEqual(fd.read(), "[]")
+    #    Base.save_to_file(None)
+    #    with open("Base.json") as fd:
+    #         self.assertEqual(fd.read(), "[]")
 
-    def testsavetofile1(self):
-        """ error save to file"""
-        with self.assertRaises(AttributeError):
-            Base.save_to_string() 
+    # def testsavetofile1(self):
+    #     """ error save to file"""
+    #     with self.assertRaises(AttributeError):
+    #         Base.save_to_string() 
 
-#    ----------------- from json --------------------------
+# #    ----------------- from json --------------------------
 
-    def testfromjson(self):
-        """ test load from json"""
-        self.assertEqual(Base.from_json_string("[]"), [])
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string(""), [])
-        lista = [1, 2, 3]
-        r = Base.to_json_string(lista)
-        self.assertEqual(Base.from_json_string(r), lista)
+#     def testfromjson(self):
+#         """ test load from json"""
+#         self.assertEqual(Base.from_json_string("[]"), [])
+#         self.assertEqual(Base.from_json_string(None), [])
+#         self.assertEqual(Base.from_json_string(""), [])
+#         lista = [1, 2, 3]
+#         r = Base.to_json_string(lista)
+#         self.assertEqual(Base.from_json_string(r), lista)
 
-    def testfromjson1(self):
-        """ error save to file"""
-        with self.assertRaises(TypeError):
-            Base.from_json_string()
+#     def testfromjson1(self):
+#         """ error save to file"""
+#         with self.assertRaises(TypeError):
+#             Base.from_json_string()
