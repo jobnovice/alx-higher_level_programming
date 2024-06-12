555fb4b67ab65edd368a5133aba599e9d95efc69
jobnovice <jobkebede47@gmail.com>
2024-06-13 02:19:14 +0300

    MM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Get all the states inside from a given database"""
-from MySQLdb import _mysql
+import MySQLdb
 import sys
 
 
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         print("Usage: <username> <password> <database name>")
         exit(1)
 
-    db = _mysql.connect(host="localhost", port=3306, user="{}",
+    db = MySQLdb._mysql.connect(host="localhost", port=3306, user="{}",
                         password="{}", database="{}"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     db.query("""select id, name from states""")
