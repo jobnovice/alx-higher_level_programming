a6049e5255e9d8d2f0276a66c14dc87b5a480bff
jobnovice <jobkebede47@gmail.com>
2024-06-13 22:53:49 +0300

    itelllu what

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Get all the states inside from a given database"""
+"""Get all the states from a given database"""
 import MySQLdb
 import sys
 
@@ -9,9 +9,12 @@ if __name__ == "__main__":
         print("Usage: <username> <password> <database name>")
         exit(1)
 
-    db = MySQLdb._mysql.connect(host="localhost", port=3306, user="{}",
-                        password="{}", database="{}"
-                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    db.query("""select id, name from states""")
-    r = db.store_result()
-    r.fetch_row()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
