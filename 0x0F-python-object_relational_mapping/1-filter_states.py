4177df4697e8c2a1cf1e8b78329d87dcda5ec61f
jobnovice <jobkebede47@gmail.com>
2024-06-15 14:52:23 +0300

    no longer needed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""lists all states that start with a specific letter"""
+"""lists all states that start with the letter N"""
 
 import MySQLdb
 import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: <username> <password> <database name>")
-    
+        exit(1)
 
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
